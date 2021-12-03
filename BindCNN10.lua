c23cd5d9e106bf10818eef537fc2901d82a6182d
KevinMcWood <55211028+KevinMcWood@users.noreply.github.com>
2021-12-04 00:05:02 +0300

    Add files via upload

--- BindCNN10.lua
+++ BindCNN10.lua
@@ -25,8 +25,8 @@ local text_buffer = imgui.ImBuffer(256)
 local sw, sh = getScreenResolution()
 local women = imgui.ImBool(false)
 
-local script_vers = 2
-local script_vers_text = "2.1"
+local script_vers = 3
+local script_vers_text = "3.0"
 
 local update_url = "https://raw.githubusercontent.com/KevinMcWood/bindcnn/main/update.ini" -- тут тоже свою ссылку
 local update_path = getWorkingDirectory() .. "/update.ini" -- и тут свою ссылку
@@ -201,9 +201,6 @@ function invv(arg)
 	end)
 end
 
-function cmd_update(arg)
-    sampShowDialog(1000, "Автообновление v2.0", "{FFFFFF}Это урок по обновлению\n{FFF000}Новая версия", "Закрыть", "", 0)
-end
 
 function imgui.OnDrawFrame()
 
